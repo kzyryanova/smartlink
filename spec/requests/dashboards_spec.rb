ba31b8bded0aa7a1929a4e9c87d454aea793ec9d
Kate Zyryanova <katrine.zv@gmail.com>
2015-11-16 12:02:54 +0200

    Add structure

--- spec/requests/dashboards_spec.rb
+++ spec/requests/dashboards_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe "Dashboards" do
-  describe "GET /dashboards" do
+describe "Dashboard" do
+
     it "should have the context SmartLink" do
       # get dashboards_path
-      visit "/dashboard/show"
+      visit root_path
       expect(page).to have_content("SmartLink")
     end
-  end
+
 end
